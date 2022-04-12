cc30f4da815c693a8a5ba70d7e605a6d35dc0b1f
hossam <hossamabubaker1996@icloud.com>
2022-04-12 23:42:23 +0300

    edit code

--- BreakingBad-Character/Controllers/ViewController.swift
+++ BreakingBad-Character/Controllers/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController,UISearchBarDelegate {
         collectionView.dataSource=self
         setupCell()
         
+        collectionView.backgroundColor = .black
   
 
         getData()
